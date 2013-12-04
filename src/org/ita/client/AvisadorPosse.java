0fd01bfaf7d217477d25b713efe52c0a92ddf2f0
Lucas Amaral <lucasamaral1@gmail.com>
2013-12-04 09:43:35 -0200

    feito avisador

--- src/org/ita/client/AvisadorPosse.java
+++ src/org/ita/client/AvisadorPosse.java
@@ -13,7 +13,7 @@ public class AvisadorPosse {
 	String hostName = "localhost";
 	int portNumber = 4567;
 	
-	public boolean avisarPosse(String fileName, int pedaco){
+	public boolean avisarPosse(String myIp, int myPort, String fileName, int pedaco){
 		try (Socket echoSocket = new Socket(hostName, portNumber);
 				PrintWriter out = new PrintWriter(echoSocket.getOutputStream(),
 						true);
@@ -22,7 +22,40 @@ public class AvisadorPosse {
 				BufferedReader in = new BufferedReader(isr);
 				BufferedReader stdIn = new BufferedReader(
 						new InputStreamReader(System.in))) {
-			//Avisar posse aqui
+			while(true){
+				System.out.println("Inicio do aviso");
+				out.println("Posse");
+				int rand = 35;
+				out.println(rand);
+				out.println(fileName);
+				out.println("pedaco:"+pedaco);
+				out.println("ip:"+myIp);
+				out.println("porta:"+myPort);
+				out.println("");
+				System.out.println("Aviso enviado");
+				
+				String resposta = in.readLine();
+				System.out.println("Reposta:" + resposta);
+				if(resposta.equals("OK")){
+					resposta = in.readLine();
+					if(Integer.parseInt(resposta) == rand){
+						System.out.println("Funcionou beleza");
+						return true;
+					}
+					else{
+						System.out.println("Rand não bateu");
+						return false;
+					}
+				} else if(resposta.equals("Repetir")) {
+					if(Integer.parseInt(resposta) == rand)
+						System.out.println("Rand bateu, repetir mensagem");
+					else {
+						System.out.println("Rand não bateu");
+						return false;
+					}
+				}
+			}
+			
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host " + hostName);
 			System.exit(1);
@@ -31,7 +64,6 @@ public class AvisadorPosse {
 					+ hostName);
 			System.exit(1);
 		}
-		
 		return false;
 	}
 
